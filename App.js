a9c3eadd4e4fb1df18dd5c1de030e0208d675252
Kevin Fergus <kevinfergus89@gmail.com>
2020-06-18 15:52:38 -0500

    inital commit

--- App.js
+++ App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	console.log('App executed');
+	let x = 1;
+	console.log('testing');
+	return (
+		<View style={styles.container}>
+			<Text>Sell sell sell</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
